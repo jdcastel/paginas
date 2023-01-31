4f5c61545a27cde901c6167e46ee1b3a70ab56e1
Juan David Castelblanco <84249113+jdcastel@users.noreply.github.com>
2023-01-30 21:28:40 -0500

    Form login class update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import { RouterProvider } from 'react-router-dom';
 import router from './navigate/Index';
 
-
-
 function App() {
   return (
     <RouterProvider router={router} />
-  );
+        );
 }
-
 export default App;
--- src/navigate/Index.js
+++ src/navigate/Index.js
@@ -1,9 +1,5 @@
-
 import ReactDOM from "react-dom/client";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Dashboard from "../components/screens/Dashboard";
 import Error404 from "../components/screens/Error404";
 import Login from "../components/screens/Login";
@@ -15,13 +11,12 @@ const router = createBrowserRouter([
   },
   {
     path: "dashboard",
-    element: <Dashboard />
-  }
-  ,
+    element: <Dashboard />,
+  },
   {
     path: "*",
-    element: <Error404 />
-  }
+    element: <Error404 />,
+  },
 ]);
 
 export default router;
